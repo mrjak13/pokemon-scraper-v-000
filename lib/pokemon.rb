586b36c2b7e9962c21b3f8e743b1e8de69cd7cf7
Justin Kenney <justinkenney13@gmail.com>
2018-10-31 19:07:14 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -22,7 +22,7 @@ class Pokemon
   def alter_hp(hp, db)
     # binding.pry
     db.execute("UPDATE pokemon SET hp = ?
-    WHERE name = ?", hp, name binding.pry)
+    WHERE name = ?", hp, name)
     # @hp = hp
 
   end
